b768316d4304df269a03844976af741f2fac7d2d
dofsofware <xamalteam@gmail.com>
2022-06-09 00:41:34 +0000

    Initial version of buntu generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/techxel/buntu/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.techxel.buntu.security;
+
+import com.techxel.buntu.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
---
+++ src/main/java/com/techxel/buntu/service/InvalidPasswordException.java
@@ -0,0 +1,10 @@
+package com.techxel.buntu.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+}
